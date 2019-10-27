1dd48914fc00f6c873f8a2f3ab97eb09ee44edfe
Railton98 <railtonleal98@gmail.com>
2019-10-27 00:13:05 -0300

    forms - values and validations

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final _formKey = GlobalKey<FormState>();
+  final feedController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +25,23 @@ class _HomePageState extends State<HomePage> {
             children: <Widget>[
               TextFormField(
                 keyboardType: TextInputType.url,
+                controller: feedController,
                 decoration: InputDecoration(labelText: "Link do RSS"),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Campo não pode ficar em branco";
+                  }
+                },
               ),
               RaisedButton(
                 child: Text("Cadastrar"),
                 color: Colors.green,
                 textColor: Colors.white,
                 onPressed: () {
-                  print("adicionou");
+                  if (_formKey.currentState.validate()) {
+                    print(feedController.text);
+                    feedController.text = '';
+                  }
                 },
               ),
             ],
