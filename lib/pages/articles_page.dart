bde473164a45cc1834bed2bdddaa58f101e8d89b
Railton98 <railtonleal98@gmail.com>
2019-11-13 00:01:12 -0300

    future builder

--- lib/pages/articles_page.dart
+++ lib/pages/articles_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advancing/data/get_feed_data.dart';
 
 class ArticlePage extends StatefulWidget {
   final String feed;
@@ -11,23 +12,60 @@ class ArticlePage extends StatefulWidget {
 
 class _ArticlePageState extends State<ArticlePage> {
   final String feed;
+  Future<List> articles;
 
   _ArticlePageState(this.feed);
 
   @override
   Widget build(BuildContext context) {
+    GetFeedData rss = GetFeedData();
+    articles = rss.read(url: this.feed);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de artigos'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text(this.feed),
-          onPressed: () {
-            // Navigator.pushNamed(context, '/third');
-          },
-        ),
+      body: FutureBuilder(
+        future: articles,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return messageTemplate(
+                title: 'Carregando lista de artigos',
+                icon: Icons.refresh,
+              );
+              break;
+            default:
+              if (snapshot.hasError) {
+                return messageTemplate(
+                  title: 'Não foi possível carregar',
+                  icon: Icons.error,
+                );
+              }
+              return messageTemplate(
+                title: 'Lista de artigos carregada',
+                icon: Icons.done,
+              );
+              break;
+          }
+        },
       ),
     );
   }
+}
+
+messageTemplate({String title, IconData icon}) {
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+        ),
+        Text(title)
+      ],
+    ),
+  );
 }
